51cc82ac158f4bd7ea34366a40b4c8ad734f3b8a
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-28 16:26:10 +0300

    added

--- Rest-Assurance/src/test/java/files/DynamicJson.java
+++ Rest-Assurance/src/test/java/files/DynamicJson.java
@@ -2,6 +2,7 @@ package files;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,16 +10,22 @@ import static io.restassured.RestAssured.given;
 public class DynamicJson {
 
 
-    @Test
-    public void addBook() {
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
         String response = given().header("Content-Type", "application/json").
-                body(files.payload.addBook("gsgırırıdfd", "awdf75aef78")).
+                body(files.payload.addBook(isbn,aisle)).
                 when().post("/Library/Addbook.php").then().log().all().assertThat().statusCode(200)
                 .extract().response().asString();
         JsonPath js = ReUseAbleMethods.rawToJson(response);
         String id = js.get("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+        return new Object[][]{{"ojfwtert", "936367"}, {"cweteeasdfa","4253t4356"}, {"okmfeasdt", "53357"}};
+
     }
 }
 
