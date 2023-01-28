9eb2505f43ff276458e120e4e3a44c9c68c6cd77
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-28 16:24:26 +0300

    added

--- Rest-Assurance/src/test/java/Basics.java
+++ Rest-Assurance/src/test/java/Basics.java
@@ -4,20 +4,24 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class Basics {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //given - all input detail
         //when - Submit the Apı -resource http method
         //then - validate response
 
         //Add place--> Update Place with New Address --> Get Place to validate if New address is present in response
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
+
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body(files.payload.addPlace()).when().post("maps/api/place/add/json")
+                .body(new String(Files.readAllBytes(Paths.get("C:\\Users\\Ali Erkam İMAM\\Desktop\\addPlace.json")))).when().post("maps/api/place/add/json")
                 .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
 
