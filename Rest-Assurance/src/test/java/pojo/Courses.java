4ff0703701a3efd714244b8e82d0b966c9be1c2b
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-02-02 17:34:10 +0300

    added

---
+++ Rest-Assurance/src/test/java/pojo/Courses.java
@@ -0,0 +1,33 @@
+package pojo;
+
+import java.util.List;
+
+public class Courses {
+    private List<WebAutomation> WebAutomation;
+    private List<Api> Api;
+    private List<Mobile> Mobile;
+
+    public List<WebAutomation>  getWebAutomation() {
+        return WebAutomation;
+    }
+
+    public void setWebAutomation(List<WebAutomation>  webAutomation) {
+        WebAutomation = webAutomation;
+    }
+
+    public List<Api> getApi() {
+        return Api;
+    }
+
+    public void setApi(List<Api> api) {
+        Api = api;
+    }
+
+    public List<Mobile> getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(List<Mobile> mobile) {
+        Mobile = mobile;
+    }
+}
